794529639d71ac984c41b966cf482bfd27e48e41
scsc <scscgit@users.noreply.github.com>
2020-03-19 22:55:33 +0100

    Added the TracingFilter

---
+++ src/main/java/eu/scscdev/dev/uniquepeople/assignment/TracingFilter.java
@@ -0,0 +1,18 @@
+package eu.scscdev.dev.uniquepeople.assignment;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+@Log
+public class TracingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("Context path: " + request.getServletContext().getContextPath());
+    }
+
+}
