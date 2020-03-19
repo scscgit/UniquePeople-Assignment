3a6f3ef00297dbc117195e25fe307eed1db5094b
scsc <scscgit@users.noreply.github.com>
2020-03-19 22:52:36 +0100

    Switched to Lombok Log

--- src/main/java/eu/scscdev/dev/uniquepeople/assignment/controllers/EmployeeController.java
+++ src/main/java/eu/scscdev/dev/uniquepeople/assignment/controllers/EmployeeController.java
@@ -3,8 +3,6 @@ package eu.scscdev.dev.uniquepeople.assignment.controllers;
 import eu.scscdev.dev.uniquepeople.assignment.dao.EmployeeDAO;
 import eu.scscdev.dev.uniquepeople.assignment.db.Employee;
 import lombok.extern.java.Log;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import javax.inject.Inject;
 import java.util.Optional;
 
-//@Log
+@Log
 public class EmployeeController {
-
-    // TODO: Let's try the Lombok instead
-    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     @Inject
     private EmployeeDAO employeeDAO;
@@ -25,10 +20,10 @@ public class EmployeeController {
     public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
         Optional<Employee> employee = employeeDAO.findOne(id);
         if (!employee.isPresent()) {
-            logger.info("Employee not found");
+            log.info("Employee not found");
             return ResponseEntity.notFound().build();
         }
-        logger.info("Employee found");
+        log.info("Employee found");
         return ResponseEntity.of(employee);
     }
 }
