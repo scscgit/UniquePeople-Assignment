a23b7d4dee8693698aa7cfdf5bffefa9ff107a6f
scsc <scscgit@users.noreply.github.com>
2020-03-19 23:31:15 +0100

    Some annotation-driven security and PUT testing for the Admin

---
+++ src/main/java/eu/scscdev/dev/uniquepeople/assignment/SecurityConfig.java
@@ -0,0 +1,46 @@
+package eu.scscdev.dev.uniquepeople.assignment;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@Log
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.profiles.active}")
+    private String profile;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (isDev()) {
+            http.csrf().disable();
+            log.warning("CSRF disabled due to DEV mode!");
+        }
+        http
+            .httpBasic().and()
+            .formLogin().and()
+            .authorizeRequests()
+            .anyRequest().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        if (isDev()) {
+            auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("most-secret")
+                .roles("ADMIN");
+        }
+    }
+
+    private boolean isDev() {
+        return "dev".equals(profile);
+    }
+}
--- src/main/java/eu/scscdev/dev/uniquepeople/assignment/controllers/EmployeeController.java
+++ src/main/java/eu/scscdev/dev/uniquepeople/assignment/controllers/EmployeeController.java
@@ -2,19 +2,27 @@ package eu.scscdev.dev.uniquepeople.assignment.controllers;
 
 import eu.scscdev.dev.uniquepeople.assignment.dao.EmployeeDAO;
 import eu.scscdev.dev.uniquepeople.assignment.db.Employee;
+import eu.scscdev.dev.uniquepeople.assignment.repository.EmployeeRepository;
 import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Log
+@Controller
 public class EmployeeController {
 
     @Inject
     private EmployeeDAO employeeDAO;
+    @Inject
+    private EmployeeRepository employeeRepository;
 
     @GetMapping("employees/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
@@ -25,5 +33,19 @@ public class EmployeeController {
         }
         log.info("Employee found");
         return ResponseEntity.of(employee);
+    }
+
+    @Secured("ADMIN")
+    @PutMapping("employees")
+    public ResponseEntity generateEmployee(HttpServletRequest request) {
+        employeeRepository.saveAndFlush(
+            Employee.builder()
+                .firstName(request.getParameter("firstName"))
+                .lastName(request.getParameter("lastName"))
+                .address(request.getParameter("address"))
+                .build()
+        );
+        log.info("Employee created");
+        return ResponseEntity.ok().build();
     }
 }
